8a45c7194f722f15f019cd03a1745889f2079568
jaweriadev <jaweriaabdulkhaliq@gmail.com>
2024-02-06 17:33:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleContent() {
+  var content = document.getElementById("content");
+  content.style.display = content.style.display === "block" ? "none" : "block";
+}
+
+function closeContent() {
+  var content = document.getElementById("content");
+  content.style.display = "none";
+}
